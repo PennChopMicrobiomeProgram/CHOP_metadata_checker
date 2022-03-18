8ed3935e091fb5788c14f2f8b4c003f924b36520
ctbus <ctbushman@gmail.com>
2022-03-18 08:17:38 -0400

    metadatacli in initial functioning state

--- metadatacli/src/metadatacli/command.py
+++ metadatacli/src/metadatacli/command.py
@@ -4,13 +4,20 @@ import os
 import shutil
 import sys
 
-def _create_project(args):
-    print(type(args))
+from .createProject import createProject
+from .db import MetadataDB
+
+def _create_project(args, db):
+    code = createProject(db, args.project_name, args.customer_name, args.customer_email)
+    url = "https://SOMEWHERE.over/the?rainbow=" + code
+    print(url)
+    return code
 
 def main(argv=None):
     p = argparse.ArgumentParser()
     p.add_argument("project_name", help="Name of the project")
     p.add_argument("customer_name", help="Name of the customer")
+    p.add_argument("customer_email", help="Contact email for the customer")
 
     args = p.parse_args(argv)
 
@@ -18,4 +25,9 @@ def main(argv=None):
     logF = open("logs/main.log", "w")
     print("Writing logs to: " + logF.name)
 
-    _create_project(args)
+    db = MetadataDB("/mnt/d/Penn/CHOP_metadata_checker/test.db")
+
+    logF.write("Creating project...")
+    code = _create_project(args, db)
+    logF.write("Project code: " + code)
+    
