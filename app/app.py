63ed3b3e9a81ce948f47988afc5e9ff6b7e3c320
ctbus <ctbushman@gmail.com>
2022-11-18 19:10:57 -0500

    Working up to S3 upload

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ app.secret_key = os.environ.get('SECRET_KEY')
 #app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 bucket = os.environ.get('BUCKET')
+db = MetadataDB(bucket)
 t = Table([], [])
 #l = Logger(os.path.join(os.environ.get('LOG_FP'), 'log.out'), os.path.join(os.environ.get('LOG_FP'), 'log.err'))
 
@@ -31,24 +32,20 @@ def wiki():
 @app.route('/review/<project_code>', methods=['GET', 'POST'])
 def review(project_code):
   if request.method == 'POST':
-    db = MetadataDB(db_fp)
-    post_review(t, db, project_code, request.form['comment'], l)
+    post_review(t, db, project_code, request.form['comment'])
   #l.log(f"Rendering final.html with project_code {project_code}.\n")
   return render_template('final.html', project_code=project_code)
 
 @app.route('/submit/<project_code>', methods=['GET', 'POST'])
 def submit(project_code):
   filename = "Select file ..."
-  # Ideally we would have one db object for the whole app, seems like routes spawn their own threads though,
-  # which sqlite doesn't play nice with
-  db = MetadataDB(db_fp)
+  project_name, client_name, client_email = db.get_project_info(project_code)
 
-  if not db.project_hash_collision(project_code):
+  if not db.code_exists(project_code):
     #l.err(f"Rendering dne.html with project_code {project_code}.\n")
     return render_template('dne.html', project_code=project_code)
   elif request.method == 'GET':
     #l.log(f"Rendering submit.html with project_code {project_code} and attachment named {filename}.\n")
-    project_name, client_name, client_email = [db.get_project_from_project_code(project_code)[x] for x in [1,2,3]]
     return render_template('submit.html', filename=filename, project_code=project_code, project_name=project_name, client_name=client_name, client_email=client_email)
   elif request.method == 'POST':
     # Check if post request has a file
@@ -71,7 +68,6 @@ def submit(project_code):
     if file_fp and allowed_file(file_fp.filename):
       global t
       t, headers, rows, highlight_missing, highlight_mismatch, highlight_repeating, highlight_not_allowed, header_issues, checks_passed = run_checks(file_fp)
-      project_name, client_name, client_email = [db.get_project_from_project_code(project_code)[x] for x in [1,2,3]]
       
       #l.log(f"Rendering submit.html with project_code {project_code} and attachment named {filename}. Includes\n\theaders: {headers}\n\trows: {rows}\n\ttable: {t}\n\tmissing: {highlight_missing}\n\tmismatch: {highlight_mismatch}\n\trepeating: {highlight_repeating}\n\tnot_allowed: {highlight_not_allowed}\n\theader_issues: {header_issues}\n\tchecks_passed: {checks_passed}\n")
       return render_template('submit.html', filename=filename, project_code=project_code, project_name=project_name, client_name=client_name, client_email=client_email, headers=headers, rows=rows, table=t, missing=highlight_missing, mismatch=highlight_mismatch, repeating=highlight_repeating, not_allowed=highlight_not_allowed, header_issues=header_issues, checks_passed=checks_passed)
