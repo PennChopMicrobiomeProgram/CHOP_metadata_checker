22ec9a9b50003a9d7f6edfe5396b5e58afc8f589
Ulthran <ctbushman@gmail.com>
2024-06-03 14:30:43 -0400

    Update env variable names to be project specific

--- app/metadatalib/src/metadatalib/__init__.py
+++ app/metadatalib/src/metadatalib/__init__.py
@@ -8,17 +8,17 @@ from sqlalchemy.orm import sessionmaker
 __version__ = "0.0.0"
 
 try:
-    db_host = os.environ["DB_HOST"]
-    db_user = os.environ["DB_USER"]
-    db_name = os.environ["DB_NAME"]
-    db_pswd = os.environ["DB_PSWD"]
+    db_host = os.environ["METADATA_DB_HOST"]
+    db_user = os.environ["METADATA_DB_USER"]
+    db_name = os.environ["METADATA_DB_NAME"]
+    db_pswd = os.environ["METADATA_DB_PSWD"]
     SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_pswd}@{db_host}/{db_name}"
 except KeyError:
     sys.stderr.write(
         "Missing database connection information in environment, using test SQLite database.\n"
     )
     sys.stderr.write(
-        f"DB_HOST: {os.environ.get('DB_HOST')}\nDB_USER: {os.environ.get('DB_USER')}\nDB_NAME: {os.environ.get('DB_NAME')}\nDB_PSWD: {os.environ.get('DB_PSWD')}\n"
+        f"METADATA_DB_HOST: {os.environ.get('METADATA_DB_HOST')}\nMETADATA_DB_USER: {os.environ.get('METADATA_DB_USER')}\nMETADATA_DB_NAME: {os.environ.get('METADATA_DB_NAME')}\nMETADATA_DB_PSWD: {os.environ.get('METADATA_DB_PSWD')}\n"
     )
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent.parent.parent.parent.resolve()}/metadata.sqlite3"
 
