0e53998e553c1db2f35bebbee4450057df61615d
Ulthran <ctbushman@gmail.com>
2024-10-10 10:06:49 -0400

    Bump version to 0.1.2

--- app/metadatalib/src/metadatalib/__init__.py
+++ app/metadatalib/src/metadatalib/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 try:
     db_host = os.environ["METADATA_DB_HOST"]
