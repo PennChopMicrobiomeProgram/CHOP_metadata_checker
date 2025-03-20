1fe83dc0a18f46d9da115cada047ff4d156768ff
Charlie <ctbushman@gmail.com>
2025-03-20 16:40:08 -0400

    Bump version to 0.3.1

--- app/metadatalib/src/metadatalib/__init__.py
+++ app/metadatalib/src/metadatalib/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 try:
     SQLALCHEMY_DATABASE_URI = os.environ["METADATA_DB_URI"]
